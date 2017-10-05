4ed341bcb553c2b072d1671be1a891d06d3b9c68
Darin Pope <darin@planetpope.com>
2017-10-05 14:05:11 -0400

    figure out orphans

--- flattener/src/main/java/Flattener.java
+++ flattener/src/main/java/Flattener.java
@@ -15,26 +15,30 @@ public class Flattener {
         int argIndex = 0;
         String topLevelDirectory = args[argIndex++];
         String targetDirectory = args[argIndex++];
+        String orphansDirectory = args[argIndex++];
         if(topLevelDirectory.endsWith("/")) {
             topLevelDirectory = topLevelDirectory.substring(0,topLevelDirectory.length()-1);
         }
         if(targetDirectory.endsWith("/")) {
             targetDirectory = targetDirectory.substring(0,targetDirectory.length()-1);
         }
-        work.start(topLevelDirectory,targetDirectory);
+        if(orphansDirectory.endsWith("/")) {
+            orphansDirectory = orphansDirectory.substring(0,orphansDirectory.length()-1);
+        }
+        work.start(topLevelDirectory,targetDirectory,orphansDirectory);
     }
 
-    private void start(final String topLevelDirectory, final String targetDirectory) throws Exception {
+    private void start(final String topLevelDirectory, final String targetDirectory, final String orphansDirectory) throws Exception {
         File[] files = new File(topLevelDirectory).listFiles();
-        showFiles(files,topLevelDirectory,targetDirectory);
+        showFiles(files,topLevelDirectory,targetDirectory,orphansDirectory);
     }
 
-    private void showFiles(File[] files,String topLevelDirectory,String targetDirectory) throws Exception {
+    private void showFiles(File[] files,String topLevelDirectory,String targetDirectory,String orphansDirectory) throws Exception {
         List<String> topLevelParts = new ArrayList<>(Arrays.asList(topLevelDirectory.split("/")));
 //        System.out.println("topLevelSize = " + topLevelParts.size());
         for (File file : files) {
             if (file.isDirectory()) {
-                showFiles(file.listFiles(),topLevelDirectory,targetDirectory);
+                showFiles(file.listFiles(),topLevelDirectory,targetDirectory,orphansDirectory);
             } else {
                 if("config.xml".equalsIgnoreCase(file.getName())) {
                     if(file.getAbsolutePath().equalsIgnoreCase(topLevelDirectory+"/config.xml")) {
@@ -49,6 +53,15 @@ public class Flattener {
                     String jobName = actualParts.get(actualParts.size()-2);
 //                    System.out.println("jobName: " + jobName);
                     if("com.cloudbees.hudson.plugins.folder.Folder".equalsIgnoreCase(doc.getDocumentElement().getNodeName())) {
+                        // always create the folder in orphans
+                        System.out.println("*********");
+                        System.out.println("type: " + doc.getDocumentElement().getNodeName());
+                        System.out.println("from: " + file.getAbsolutePath());
+                        System.out.println("  or: " + orphansDirectory + "/jobs/" + jobName + "/" +file.getName());
+                        System.out.println("*********");
+//                        Files.createDirectories(Paths.get(specificTargetDirectory + "/jobs/" + jobName));
+//                        Files.copy(file.toPath(),(new File(specificTargetDirectory + "/jobs/" + jobName + "/" +file.getName())).toPath());
+
                         // is this a top level folder?
 //                        System.out.println("actualPartsSize = " + actualParts.size());
                         if(topLevelParts.size() + 3 == actualParts.size()) {
@@ -80,7 +93,12 @@ public class Flattener {
                         try {
                             Files.copy(file.toPath(), (new File(specificTargetDirectory + "/jobs/" + jobName + "/" + file.getName())).toPath());
                         } catch (java.nio.file.FileAlreadyExistsException e) {
-                            //TODO: send this to orphans
+                            specificTargetDirectory = orphansDirectory + "";
+                            System.out.println("*********");
+                            System.out.println("type: " + doc.getDocumentElement().getNodeName());
+                            System.out.println("from: " + file.getAbsolutePath());
+                            System.out.println("  to: " + specificTargetDirectory + "/jobs/" + jobName + "/" +file.getName());
+                            System.out.println("*********");
                         }
                     }
                 }
