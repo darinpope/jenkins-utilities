90fa57b7233310c4a3bcdb7551d3f31a421b3edf
Darin Pope <darin@planetpope.com>
2017-10-05 12:12:40 -0400

    quick rough in to test

--- flattener/src/main/java/Flattener.java
+++ flattener/src/main/java/Flattener.java
@@ -1,14 +1,30 @@
+import java.io.File;
+
 public class Flattener {
     public static void main(final String... args) throws Exception {
         Flattener work = new Flattener();
         int argIndex = 0;
         String topLevelDirectory = args[argIndex++];
-        work.start(topLevelDirectory);
+        String targetDirectory = args[argIndex++];
+        work.start(topLevelDirectory,targetDirectory);
     }
 
-    private void start(final String topLevelDirectory) throws Exception {
+    private void start(final String topLevelDirectory, final String targetDirectory) throws Exception {
         System.out.println(topLevelDirectory);
+        File[] files = new File(topLevelDirectory).listFiles();
+        showFiles(files,targetDirectory);
+    }
 
-
+    private void showFiles(File[] files,String targetDirectory) {
+        for (File file : files) {
+            if (file.isDirectory()) {
+                System.out.println("Directory: " + file.getName());
+                showFiles(file.listFiles(),targetDirectory);
+            } else {
+                if("config.xml".equalsIgnoreCase(file.getName())) {
+                    System.out.println(file.getName());
+                }
+            }
+        }
     }
 }
